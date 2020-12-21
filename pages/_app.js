5794924bd887a5bcec91d619ae3356a552f07329
Bao Hung Duong <baohungdev@icloud.com>
2020-12-21 11:02:52 +0700

    add gg gg analytics

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,20 @@ const App = ({ Component, pageProps }) => (
     <Head>
       <title>{"Duong Bao Hung Personal Website"}</title>
       <link href="/public/assets/NextJS.png" />
+      {/* Global site tag (gtag.js) - Google Analytics  */}
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-8YPLPTHQLZ"
+      />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-8YPLPTHQLZ');`,
+        }}
+      />
     </Head>
     <ColorModeProvider value={"light"}>
       <Component {...pageProps} />
