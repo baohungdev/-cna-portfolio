ca774145b0936d57d4eae1eebe6546b609b228d0
working <working@Duongs-MacBook-Pro.local>
2021-04-17 16:25:25 +0700

    edit linkedin profile url

--- public/constants/urls.js
+++ public/constants/urls.js
@@ -1,4 +1,3 @@
 export const GITHUB_LINK = "https://github.com/baohungdev";
-export const LINKED_IN_LINK =
-  "https://www.linkedin.com/in/hung-duong-7382741a7/";
+export const LINKED_IN_LINK = "https://www.linkedin.com/in/baohungdev/";
 export const EMAIL_LINK = "mailto:baohungdev@icloud.com";
